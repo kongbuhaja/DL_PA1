ddc004d49ac582b9cc3928e001aa0f0fc2e9c077
kongbuhaja <pcar530@gamil.com>
2024-04-30 05:31:44 +0000

    update dockerfile

--- dockerfiles/pa1.sh
+++ dockerfiles/pa1.sh
@@ -1,2 +1,2 @@
 sudo docker build --force-rm -f pa1.dockerfile -t pa1:1.0 .
-sudo docker run --gpus all --cpuset-cpus=0-31 -m 120g --shm-size=16g -it -v /home/dblab/hs/DL_PA1:/home/DL_PA1 --name dl_pa1 pa1:1.0
+sudo docker run --gpus "all" --cpuset-cpus=0-15 -m 60g --shm-size=8g -it -v /home/hs/ML/DL_PA1:/home/DL_PA1 --name dl_pa1 pa1:1.0
