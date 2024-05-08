8a53ceae18a24b4d092d3cc16e48a0418c88e130
kongbuhaja <pcar530@gmail.com>
2024-05-08 03:57:34 +0000

    update config

--- src/config.py
+++ src/config.py
@@ -2,15 +2,15 @@ import os
 
 # Training Hyperparameters
 NUM_CLASSES                 = 200
-BATCH_SIZE                  = 128
+BATCH_SIZE                  = 64
 VAL_EVERY_N_EPOCH           = 1
 
 METRIC                      = 'f1score'
 
-NUM_EPOCHS                  = 500
-OPTIMIZER_PARAMS            = {'type': 'SGD', 'lr': 0.1, 'momentum': 0.9}
+NUM_EPOCHS                  = 1000
+OPTIMIZER_PARAMS            = {'type': 'SGD', 'lr': 0.01, 'momentum': 0.9}
 # OPTIMIZER_PARAMS            = {'type': 'AdamW', 'lr': 0.01}
-SCHEDULER_PARAMS            = {'type': 'MultiStepLR', 'milestones': [100, 250, 400], 'gamma': 0.5}
+SCHEDULER_PARAMS            = {'type': 'MultiStepLR', 'milestones': [100, 200, 400, 600], 'gamma': 0.5}
 
 # Dataset
 DATASET_ROOT_PATH           = 'datasets/'
@@ -41,12 +41,12 @@ WANDB_IMG_LOG_FREQ          = 50
 WANDB_NAME                  = f'{MODEL_NAME}-B{BATCH_SIZE}-{OPTIMIZER_PARAMS["type"]}'
 
 if MODEL_NAME == 'ViT':
-    PATCH_SIZE                  = 8
-    HIDDEN_SIZE                 = 768
+    PATCH_SIZE                  = 4
+    HIDDEN_SIZE                 = 384
     MSA_DROPOUT_RATE            = 0.0
-    HEAD_SIZE                   = 8
-    MLP_SIZE                    = 2000
-    TRANSFORMER_SIZE            = 12
+    HEAD_SIZE                   = 4
+    MLP_SIZE                    = 1000
+    TRANSFORMER_SIZE            = 6
     TRANSFORMER_DROPOUT_RATE    = 0.1
     WANDB_NAME                 += f'-T{TRANSFORMER_SIZE}-P{PATCH_SIZE}-H{HIDDEN_SIZE}-M{MLP_SIZE}'
 
